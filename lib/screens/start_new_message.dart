7d8823db87f71117318f6e5d80999f4920ba61bd
aditechdev <aditechdevofficial@gmail.com>
2022-05-29 08:52:29 +0530

    :sparkles: Card option done

--- lib/screens/start_new_message.dart
+++ lib/screens/start_new_message.dart
@@ -12,6 +12,7 @@ class StartNewMessageScreen extends StatefulWidget {
 class _StartNewMessageScreenState extends State<StartNewMessageScreen> {
   bool boolShowMyLanguageList = false;
   bool boolShowLearningLanguageList = false;
+
   List languageList = [
     "English",
     "Hindi",
@@ -25,8 +26,14 @@ class _StartNewMessageScreenState extends State<StartNewMessageScreen> {
     "Bhojpuri",
     "Korean"
   ];
+  List<Map<String, String>> scrollImage = [
+    {'image': "assets/images/restro.jpeg", 'label': "Restaurant"},
+    {'image': "assets/images/petrolpump.jpeg", 'label': 'Petrol Pump'},
+    {'image': "assets/images/hospital.jpeg", 'label': 'Hospital'},
+  ];
   String selectMyLanguage = "Select Language";
   String selectLearningLanguage = "Select Language";
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -91,27 +98,45 @@ class _StartNewMessageScreenState extends State<StartNewMessageScreen> {
         ),
         body: Stack(
           children: [
+            // const SizedBox(
+            //   height: 40,
+            // ),
             Column(
               children: [
-                Text(
-                  "Please Select My Language",
-                  style: TextStyle(
-                    color: Colors.red[300],
-                  ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  "Select One option",
+                  style: TextStyle(fontSize: 20, color: Colors.black87),
                 ),
                 const SizedBox(
                   height: 40,
                 ),
-                const MultiCircle(),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Bience",
-                  style: appBarTitleStyle,
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      scrollImage.length,
+                      (index) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                        },
+                        child: ScrollImage(
+                          image: scrollImage[index]['image'].toString(),
+                          bottomPadding: index == scrollImage.length - 1,
+                          title: scrollImage[index]['label'].toString(),
+                          selectedIcon: selectedIndex == index,
+                        ),
+                      ),
+                    ),
+                  ),
                 )
               ],
             ),
+
             boolShowMyLanguageList
                 ? Positioned(
                     left: 55,
@@ -163,6 +188,81 @@ class _StartNewMessageScreenState extends State<StartNewMessageScreen> {
           icon: const Icon(Icons.send),
           label: Text("STart".toUpperCase()),
         ));
+  }
+}
+
+class ScrollImage extends StatelessWidget {
+  final String image;
+  final bool bottomPadding;
+  final String title;
+  final bool selectedIcon;
+
+  const ScrollImage({
+    Key? key,
+    required this.image,
+    required this.bottomPadding,
+    required this.title,
+    this.selectedIcon = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double cardSize = 150;
+    return Padding(
+      padding: EdgeInsets.only(left: 32, right: bottomPadding ? 50 : 0),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(125)),
+        clipBehavior: Clip.hardEdge,
+        child: Stack(
+          children: [
+            Container(
+              height: cardSize,
+              width: cardSize,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.red),
+              child: Image.asset(
+                image,
+                fit: BoxFit.cover,
+                height: cardSize,
+                width: cardSize,
+              ),
+            ),
+            Container(
+              height: cardSize,
+              width: cardSize,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color.fromARGB(255, 0, 0, 0)
+                    .withOpacity(selectedIcon ? 0.8 : 0.6),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    selectedIcon
+                        ? Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.verified,
+                              color: Colors.green[400],
+                              size: 30,
+
+                              // style: const TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          )
+                        : Container(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
