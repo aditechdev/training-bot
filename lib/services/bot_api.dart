a7fc20e53cb6295fe97644bbca1477b885184970
aditechdev <aditechdevofficial@gmail.com>
2022-05-31 12:23:10 +0530

    :seedling: Added API File

--- lib/services/bot_api.dart
+++ lib/services/bot_api.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class BotApi {
+  Future<Map> fetchBotMessage() async {
+    var client = http.Client();
 
+    try {
+      final response = await client.get(
+          Uri.parse(
+              "https://my-json-server.typicode.com/tryninjastudy/dummyapi/db"),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+          });
 
-  
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body) as Map;
+      } else {
+        throw "Something went wrong";
+      }
+    } catch (e) {
+      throw "Something went wrong";
+    }
+  }
 }
