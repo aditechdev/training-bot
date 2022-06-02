0694281b15110f3b70155c36ee251a280443416a
aditechdev <aditechdevofficial@gmail.com>
2022-06-02 11:48:21 +0530

    :sparkles: Trend Page Introduse

---
+++ lib/screens/trend_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrendPage extends StatelessWidget {
+  const TrendPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Tranning Page"),
+    );
+  }
+}
